607bdbeb5d257869e19a2baf78fe1b915933bd72
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-09-25 13:20:55 -0400

    updates

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -17,12 +17,13 @@ const Contact = () => {
     <div className={`contact-body ${isLoaded ? 'fade-in' : ''}`}>
       <div className="contact-container">
         <div className="contact-info">
-          <p>Email: vaibhav@example.com</p>
-          <p>Phone: +91-1234567890</p>
+          <p>Email: ivaibhavkalia@gmail.com</p>
+          <p>Phone: +1(289)395-2714</p>
         </div>
         <form>
           <input type="text" placeholder="Your Name" required />
           <input type="email" placeholder="Your Email" required />
+          <input type="text" placeholder="Your phone number" required />
           <textarea placeholder="Your Message" required></textarea>
           <button type="submit">Send</button>
         </form>
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -3,14 +3,46 @@
     Date: - 2024-09-25
     subject: - Comp229-Web Application Development*/
 
-    
+
 import React from 'react';
+import './Projects.css';
 
 const Projects = () => {
   return (
-    <div>
+    <div className="projects-container">
       <h2>My Projects</h2>
-      <p>[List your projects here]</p>
+      <div className="projects-list">
+        <div className="project-item">
+          <img 
+            src={`${process.env.PUBLIC_URL}/images/weather.jpg`} 
+            alt="Project 1" 
+            className="project-image" 
+          />
+          <h2>Weather application</h2>
+          <p>I have created a weather application when i was in high school. This weather application provides users with real-time weather updates and forecasts for any location worldwide. It features an intuitive user interface that allows users to search for cities, view current weather conditions, and access a detailed forecast for the coming days. </p>
+        </div>
+        <div className="project-item">
+          <img 
+            src={`${process.env.PUBLIC_URL}/images/coffeeApp.jpg`} 
+            alt="Project 2" 
+            className="project-image" 
+          />
+          <h2>Coffee Shop mobile application</h2>
+          <p>Working as a barista in my pastimes, i tried to help my employers by creating a mobile application that helps them to accept orders online now.</p>
+        </div>
+        <div className="project-item">
+        <img 
+            src={`${process.env.PUBLIC_URL}/images/to do list.png`} 
+            alt="Project 3" 
+            className="project-image" 
+          />
+          <h2>To-Do list application</h2>
+          <p>There are a lot of to do list applications available but i needed somthing that completely understands what i require. I created this application to link it directly to my college time-table and my workplace schedule.
+            Any new activity that i add to my time-table or my workplace schedule is automatically added to my to-do list.
+          </p>
+          
+        </div>
+      </div>
     </div>
   );
 };
